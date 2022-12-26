507d2edd78f7a2309c358b9dba8436aba0c3b90d
Ahmet İ. Koca <info@xtechnology.co>
2022-12-26 17:59:51 +0300

    x

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,17 @@
-import Container from "../components/container";
-import HeroPost from "../components/hero-post";
-import Intro from "../components/intro";
-import Layout from "../components/layout";
-import MoreStories from "../components/more-stories";
-import { getAllPostsForHome } from "../lib/api";
-import { GetStaticProps } from "next";
-
+import Container from "../components/container"
+import HeroPost from "../components/hero-post"
+import Intro from "../components/intro"
+import Layout from "../components/layout"
+import MoreStories from "../components/more-stories"
+import { getAllPostsForHome } from "../lib/api"
+import { GetStaticProps } from "next"
 
 export default function Index({ allPosts: { edges }, preview }) {
-	const heroPost = edges[0]?.node;
-	const morePosts = edges.slice(1);
+	const heroPost = edges[0]?.node
+	const morePosts = edges.slice(1)
 
-	return (	
-		<Layout {...preview}>			
+	return (
+		<Layout {...preview}>
 			<Container>
 				<Intro />
 				{heroPost && (
@@ -28,14 +27,14 @@ export default function Index({ allPosts: { edges }, preview }) {
 				{morePosts.length > 0 && <MoreStories posts={morePosts} />}
 			</Container>
 		</Layout>
-	);
+	)
 }
 
 export const getStaticProps: GetStaticProps = async ({ preview = false }) => {
-	const allPosts = await getAllPostsForHome(preview);
+	const allPosts = await getAllPostsForHome(preview)
 
 	return {
 		props: { allPosts, preview },
 		revalidate: 10,
-	};
-};
+	}
+}
