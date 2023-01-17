63e88b90044e3aee69ed33df876219c946437ca1
xtechnology <87781518+XTechnology-TR@users.noreply.github.com>
2023-01-17 23:40:38 +0300

    Update meta.tsx
    Signed-off-by: xtechnology <87781518+XTechnology-TR@users.noreply.github.com>

--- components/meta.tsx
+++ components/meta.tsx
@@ -4,7 +4,20 @@ import { CMS_NAME, HOME_OG_IMAGE_URL } from '../lib/constants'
 export default function Meta() {
   return (
     <Head>
-      <link
+      <link rel="shortcut icon" href="/favicon/favicon.ico" />
+      <meta name="msapplication-TileColor" content="#000000" />
+      <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
+      <meta name="theme-color" content="#000" />
+			<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  	  <meta http-equiv="x-ua-compatible" content="ie=edge" />
+			<meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no, shrink-to-fit=no" />
+			<meta name="format-detection" content="telephone=no" />
+      <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
+      <meta
+        name="description" content="Bilim ve Teknoloji alandında kendini geliştirmek isteyenlerin ilgi duyacağı makale, eğitim ve haberleri bu bölümden takip edebilirsiniz.
+        ${CMS_NAME}" />
+      <meta property="og:image" content={HOME_OG_IMAGE_URL} />
+			    <link
         rel="apple-touch-icon"
         sizes="180x180"
         href="/favicon/apple-touch-icon.png"
@@ -27,17 +40,6 @@ export default function Meta() {
         href="/favicon/safari-pinned-tab.svg"
         color="#000000"
       />
-      <link rel="shortcut icon" href="/favicon/favicon.ico" />
-      <meta name="msapplication-TileColor" content="#000000" />
-      <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
-      <meta name="theme-color" content="#000" />
-      <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
-      <meta
-        name="description"
-        content={`Blog: Bilim ve Teknoloji alandında kendini geliştirmek isteyenlerin ilgi duyacağı makale, eğitim ve haberleri bu bölümden takip edebilirsiniz.
-        ${CMS_NAME}.`}
-      />
-      <meta property="og:image" content={HOME_OG_IMAGE_URL} />
     </Head>
   )
 }
