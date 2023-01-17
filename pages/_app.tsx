eb76811b91fdb8dc7270fd57f6dd2907570ee57d
xtechnology <87781518+XTechnology-TR@users.noreply.github.com>
2023-01-17 23:59:52 +0300

    Update _app.tsx
    Signed-off-by: xtechnology <87781518+XTechnology-TR@users.noreply.github.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,7 @@ import { Inter } from '@next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 
-export default function MyApp({ Component, pageProps }: AppProps) {
+export default function MyApp({ Component, pageProps }) {
   return (
     <main className={inter.className}>
       <Component {...pageProps} />
