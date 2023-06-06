efea70f98f1ee579d6a0f40f935231a29adcf8c7
xtechnology <87781518+XTechnology-TR@users.noreply.github.com>
2023-06-06 15:48:51 +0300

    Update sitemap.xml.js
    Signed-off-by: xtechnology <87781518+XTechnology-TR@users.noreply.github.com>

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -1,15 +1,15 @@
 //pages/sitemap.xml.js
-const EXTERNAL_DATA_URL = 'https://jsonplaceholder.typicode.com/posts';
+const EXTERNAL_DATA_URL = 'https://blog.xtechnology.co/posts';
 
 function generateSiteMap(posts) {
   return `<?xml version="1.0" encoding="UTF-8"?>
    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
      <!--We manually set the two URLs we know already-->
      <url>
-       <loc>https://jsonplaceholder.typicode.com</loc>
+       <loc>https://blog.xtechnology.co</loc>
      </url>
      <url>
-       <loc>https://jsonplaceholder.typicode.com/guide</loc>
+       <loc>https://blog.xtechnology.co/dev</loc>
      </url>
      ${posts
        .map(({ id }) => {
