681b01ba1a9ca566badf9723eaa1bf49e93093dc
XTechnology-TR <info@xtechnology.co>
2023-09-22 00:11:14 +0300

    x1

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from "next/app";
 import { Analytics } from "@vercel/analytics/react";
 import "../styles/index.css";
+import '../styles/global.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,9 @@
-// postcss.config.js
+// If you want to use other PostCSS plugins, see the following:
+// https://tailwindcss.com/docs/using-with-preprocessors
 module.exports = {
-	plugins: {
-		"postcss-import": {},
-		tailwindcss: {},
-		autoprefixer: {},
-	},
-};
+  plugins: {
+    'postcss-import': {},
+    tailwindcss: {},
+    autoprefixer: {},
+  }
+}
